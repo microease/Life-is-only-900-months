bc3502fdb3e2356638c5d6f967d8a1355ef98221
胡小易 <microease@163.com>
2018-06-25 08:26:00 +0800

    add

--- h5view.php
+++ h5view.php
@@ -1,43 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>人生只有900个月-我在写代码</title>
-  <link rel="stylesheet" href="normalize.css">
-  <style>
-    body { font-size: 12px; }
-    li{
-      list-style: none;
-    }
-    .lifetime{
-      width: auto;
-      height: auto;
-      margin: 0 auto;
-      overflow: hidden; 
-    }
-    .m900{
-      background-color: #fff;
-    }
-    .m{
-      width:3.1%;
-      height: 18px;
-      border:1px solid #ccc;
-      float: left;
-      background-color: green;
-    }
-    .past{
-      background-color: #000;
-    }
-    .description { margin-top: 50px; }
-  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>人生只有900个月-我在写代码</title>
+    <link rel="stylesheet" href="normalize.css">
+    <style>
+        body {
+            font-size: 12px;
+        }
+
+        li {
+            list-style: none;
+        }
+
+        .lifetime {
+            width: auto;
+            height: auto;
+            margin: 0 auto;
+            overflow: hidden;
+        }
+
+        .m900 {
+            background-color: #fff;
+        }
+
+        .m {
+            width: 3.1%;
+            height: 18px;
+            border: 1px solid #ccc;
+            float: left;
+            background-color: green;
+        }
+
+        .past {
+            background-color: #000;
+        }
+
+        .description {
+            margin-top: 50px;
+        }
+    </style>
 </head>
 <body>
-<?php 
+<?php
 $userdata = $_POST["birthday"];
 date_default_timezone_set('PRC');
-$arr = explode("-",$userdata); 
+$arr = explode("-", $userdata);
 $useryear = $arr[0];
 $usermonth = $arr[1];
 $thisyear = date('Y');
@@ -47,24 +57,24 @@ $usermonth = (int)$usermonth;
 $thisyear = (int)$thisyear;
 $thismonth = (int)$thismonth;
 $mathyear = $thisyear - $useryear;
-$mathyearmonth = $mathyear *12;
+$mathyearmonth = $mathyear * 12;
 $mathmonth = $thismonth - $usermonth;
 $mathmonth += $mathyearmonth;
 $leftmonth = 900 - $mathmonth;
 ?>
-  <div class="lifetime">
+<div class="lifetime">
     <div class="m900">
-    <?php 
+        <?php
         // echo "<li class='m past'></li>"*44; 
-        echo str_repeat('<li class="m past"></li>',$mathmonth);
-        echo str_repeat('<li class="m"></li>',$leftmonth);
+        echo str_repeat('<li class="m past"></li>', $mathmonth);
+        echo str_repeat('<li class="m"></li>', $leftmonth);
         ?>
     </div>
-  </div>
-    <div class="description">
-        已经使用<?php echo$mathmonth?>月，剩余<?php echo$leftmonth?>月，共900月。 <a href="/900">返回</a><br>
-        本表格：45*20
-      </div>
-  
+</div>
+<div class="description">
+    已经使用<?php echo $mathmonth ?>月，剩余<?php echo $leftmonth ?>月，共900月。 <a href="/900">返回</a><br>
+    本表格：45*20
+</div>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,37 +1,67 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <title>人生只有900个月-我在写代码</title>
     <style type="text/css">
-      body { font-size: 12px; }
-      #container { position: relative; width: 900px; height: auto; overflow: hidden; margin: 0 auto; text-align: center; }
-      #introduction { margin-top: 90px auto;color:red;}
-      .form { background-color: #EEE; width: 900px; height: auto; padding: 10px; margin: 20px auto 0; overflow: hidden; }
-      .thanks{margin-top:100px;} 
-      .author { margin-top: 15px; }
+        body {
+            font-size: 12px;
+        }
+
+        #container {
+            position: relative;
+            width: 900px;
+            height: auto;
+            overflow: hidden;
+            margin: 0 auto;
+            text-align: center;
+        }
+
+        #introduction {
+            margin-top: 90px auto;
+            color: red;
+        }
+
+        .form {
+            background-color: #EEE;
+            width: 900px;
+            height: auto;
+            padding: 10px;
+            margin: 20px auto 0;
+            overflow: hidden;
+        }
+
+        .thanks {
+            margin-top: 100px;
+        }
+
+        .author {
+            margin-top: 15px;
+        }
     </style>
-  </head>
-  <body>
-    <div id="container">
-      <div id="introduction">
-      <a href="http://www.wzxdm.com"><img src="http://www.wzxdm.com/wp-content/uploads/2018/05/path.jpg" alt=""></a>
+</head>
+<body>
+<div id="container">
+    <div id="introduction">
+        <a href="http://www.wzxdm.com"><img src="http://www.wzxdm.com/wp-content/uploads/2018/05/path.jpg" alt=""></a>
         你知道吗？人生只有900个月！输入出生日期（例如：1986-1-1），看看你的时间使用情况。
-      </div>
-      <div class="form">
-        <form action="http://www.wzxdm.com/900/view.php" method="post">
-          出生日期 <input type="text" name="birthday" size="10" value="1995-05-21"/> <input type="submit" value="提交" />
-        </form>
-      </div>
-      <div class="thanks">
-        灵感来自于这篇博客<a href="http://www.ruanyifeng.com/blog/2011/05/900-month_lifespan.html" target="_blank">《人生只有900个月》</a>。
-      </div>
-      <div class="author">
-        作者：<a href="https://github.com/microease">MicroEase</a>
-      </div>
-      <div class="thistime">
-        <?date_default_timezone_set('PRC');echo "当前时间:".date('Y-m-d');?>
-      </div>
     </div>
-  </body>
+    <div class="form">
+        <form action="http://www.wzxdm.com/900/view.php" method="post">
+            出生日期 <input type="text" name="birthday" size="10" value="1995-05-21"/> <input type="submit" value="提交"/>
+        </form>
+    </div>
+    <div class="thanks">
+        灵感来自于这篇博客<a href="http://www.ruanyifeng.com/blog/2011/05/900-month_lifespan.html"
+                    target="_blank">《人生只有900个月》</a>。
+    </div>
+    <div class="author">
+        作者：<a href="https://github.com/microease">MicroEase</a>
+    </div>
+    <div class="thistime">
+        <? date_default_timezone_set('PRC');
+        echo "当前时间:" . date('Y-m-d'); ?>
+    </div>
+</div>
+</body>
 </html>
